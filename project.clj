91a4ada069b31f7e28b5307e9cc873bacc444070
vinay427 <vhiremath95@gmail.com>
2015-06-24 23:59:28 -0400

    Added things

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
+                 [cheshire "5.5.0"]
                  [http-kit "2.1.16"]])
--- src/rendezvous_clj/core.clj
+++ src/rendezvous_clj/core.clj
@@ -1,9 +1,16 @@
 (ns rendezvous-clj.core
   (:require [compojure.core :refer :all]
+            [cheshire.core :refer :all]
             [org.httpkit.server :refer [run-server]]))
 
+(defn meeting-handler
+  [data]
+  (let [data (parse-string data)]
+    (str data)))
+
 (defroutes rendezvous-clj
-  (GET "/" [] "Hello world!"))
+  (GET "/meeting/:data" [data] (meeting-handler data))
+  (GET "//:name" [name] (str "Hello " name)))
 
 (defn -main []
   (run-server rendezvous-clj {:port 8080}))
